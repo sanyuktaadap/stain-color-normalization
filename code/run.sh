8c2f8c0548edb1dcd3583ba0c2231109b05538ff
sanyuktaadap <sanyuadap@gmail.com>
2024-04-22 16:33:59 -0400

    Cleaning code

--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-# #####################################################
+
 # Images, image maps, tissue to exclude, and output dataframe name
-# #####################################################
 
 Image_Array=($(ls ./data/for_normalization/Images))
 Image_Map_Array=($(ls ./data/for_normalization/Image_Maps/))
@@ -38,31 +37,26 @@ done
 
 All_Images_Array=($(ls ./data/Images))
 
-# #####################################################
 # Other variables
-# #####################################################
 Training_Time=10
 Knuth_Bin_Size=4096
-# #####################################################
+
 # Create Directories (Ocean Code requirement)
-# #####################################################
 mkdir -p ./results/Images_Histograms_DataFrames
 mkdir -p ./results/Images_Stain_Stats_DataFrames
 mkdir -p ./results/Normalization_Parameters
 mkdir -p ./results/Normalized_Images
 mkdir -p ./results/Normalization_Parameters/${#Image_Array[@]}_Image_Cohort_Aggregated_Normalization_Parameters
-# #####################################################
+
 # File Paths
-# #####################################################
 Python_Scripts_Directory="./code/src/"
 Images_Directory="./data/Images/"
 Image_Maps_Directory="./data/for_normalization/Image_Maps/"
 Gray_Level_Labels_Directory="./data/Csv_Files/"
 Output_Files="./results/"
-# #####################################################
-#   1) Calculate stain vectors and histogram for each
-#      image and store info in a dataframe
-#######################################################
+
+# 1) Calculate stain vectors and histogram for each image and store info in a dataframe
+
 for i in ${!Image_Array[@]}; do
     echo "----------------------------------------------------"
     echo "Generate pandas dataframes containing stain vectors "
@@ -78,10 +72,8 @@ for i in ${!Image_Array[@]}; do
     --Stain_Vector_Training      $Training_Time
 done
 
-#####################################################
-#   2) Aggregate stain vectors and histogram from four
-#      images in step 1
-#####################################################
+# 2) Aggregate stain vectors and histogram from the images in step 1
+
 echo "----------------------------------------------------"
 echo "Aggregate stain vectors and histograms              "
 echo "----------------------------------------------------"
@@ -91,10 +83,7 @@ python $Python_Scripts_Directory"2-Aggregate_Stain_Vectors_and_Histograms.py" \
 --Output_Directory                 $Output_Files"Normalization_Parameters" \
 --Number_of_Images                 ${#Image_Array[@]}
 
-# #####################################################
-#   3) Normalize each image using aggregated stain vectors
-#      and histogram in step 2
-# #####################################################
+# 3) Normalize each image using aggregated stain vectors and histogram in step 2
 
 for i in ${!All_Images_Array[@]}; do
     echo "----------------------------------------------------"
