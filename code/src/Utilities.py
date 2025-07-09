2ebd5e6a87dcb6e392d264be046ff1260de8a24b
sanyuktaadap <sanyuadap@gmail.com>
2025-07-09 15:00:39 -0400

    using original versions of CalculateStainVector and CalculateDensityMap functions, but processing the image in chunks instead of whole image at once.

--- code/src/Utilities.py
+++ code/src/Utilities.py
@@ -12,6 +12,7 @@ import spams
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.linear_model import Lasso
 import psutil
+from tqdm import tqdm
 
 WHITE_COLOR                       = 255
 INFERRED_DIMENSION                = -1
@@ -96,96 +97,86 @@ def SortOutStainVectors(StainVectors):
     return HandE_StainVectors
 
 
-def CalculateStainVector(Image,Stain_Vector_Lambda,Stain_Vector_Training_Time):
-    """
-    Get 2x3 stain matrix. First row H and second row E
-    :param I:
-    :param threshold:
-    :param ld:
-    :return:
-    Use smaller values of the lambda (0.01-0.1) for better reconstruction.
-    However, if the normalized image seems not fine, increase or decrease the value accordingly.
-    """
-    NUMBER_OF_STAINS = 2
-    X                = rgb2od(Image).reshape((INFERRED_DIMENSION, NUMBER_OF_COLORS))
-    StainVectors = spams.trainDL(
-        X        = X.T,
-        K        = NUMBER_OF_STAINS,
-        lambda1  = Stain_Vector_Lambda,
-        mode     = 2,
-        modeD    = 0,
-        posAlpha = True,
-        iter     = -Stain_Vector_Training_Time,
-        posD     = True,
-        verbose  = False
-    ).T
-    print('Normalize vector')
-    StainVectors      = normalize_rows(StainVectors)
-
-    print('need to find H&E order by looking at color ratios')
-    StainVectorOutput = SortOutStainVectors(StainVectors)
-
-    return StainVectorOutput
-
-
-# def CalculateStainVector(Image, Stain_Vector_Lambda, Stain_Vector_Training_Time):
+# def CalculateStainVector(Image,Stain_Vector_Lambda,Stain_Vector_Training_Time):
 #     """
-#     Get 2x3 stain matrix. First row H and second row E.
-#     :param Image: Input RGB image as a numpy array.
-#     :param Stain_Vector_Lambda: Sparsity controlling parameter.
-#     :param Stain_Vector_Training_Time: Not directly used due to scikit-learn's API, but you can adjust the 'n_iter' parameter of MiniBatchDictionaryLearning if needed.
-#     :return: A 2x3 matrix of stain vectors for Hematoxylin and Eosin.
+#     Get 2x3 stain matrix. First row H and second row E
+#     :param I:
+#     :param threshold:
+#     :param ld:
+#     :return:
+#     Use smaller values of the lambda (0.01-0.1) for better reconstruction.
+#     However, if the normalized image seems not fine, increase or decrease the value accordingly.
 #     """
 #     NUMBER_OF_STAINS = 2
-#     # Assuming rgb2od and other necessary functions are defined elsewhere
-#     X = rgb2od(Image).reshape((INFERRED_DIMENSION, NUMBER_OF_COLORS))  # Reshape the optical density matrix
+#     X                = rgb2od(Image).reshape((INFERRED_DIMENSION, NUMBER_OF_COLORS))
+#     StainVectors = spams.trainDL(
+#         X        = X.T,
+#         K        = NUMBER_OF_STAINS,
+#         lambda1  = Stain_Vector_Lambda,
+#         mode     = 2,
+#         modeD    = 0,
+#         posAlpha = True,
+#         iter     = -Stain_Vector_Training_Time,
+#         posD     = True,
+#         verbose  = False
+#     ).T
+#     print('Normalize vector')
+#     StainVectors      = normalize_rows(StainVectors)
 
-#     # Initialize the MiniBatchDictionaryLearning model
-#     model = MiniBatchDictionaryLearning(n_components=NUMBER_OF_STAINS,
-#                                         alpha=Stain_Vector_Lambda,
-#                                         max_iter=Stain_Vector_Training_Time,
-#                                         fit_algorithm='lars',
-#                                         positive_code=False,  # This option is not available, but post-processing could be applied
-#                                         transform_algorithm='lasso_lars',
-#                                         random_state=0)
-
-#     # Fit the model and retrieve the components (stain vectors)
-#     model.fit(X)
-#     StainVectors = model.components_
-
-#     # Normalize each stain vector
-#     StainVectors = normalize_rows(StainVectors)
-
-#     # Post-process StainVectors to ensure positivity if necessary
-#     # For example: StainVectors[StainVectors < 0] = 0
-
-#     # Assuming SortOutStainVectors function is defined elsewhere and performs necessary post-processing
+#     print('need to find H&E order by looking at color ratios')
 #     StainVectorOutput = SortOutStainVectors(StainVectors)
 
 #     return StainVectorOutput
 
 
-# def CalculateDensityMap(Image, StainMatrix,lamda):
-#     """
-#     Get concentrations, a npix x 2 matrix
-#     :param I:
-#     :param stain_matrix: a 2x3 six
-#     :return:
-#     """
+def CalculateStainVector(Image, Stain_Vector_Lambda, Stain_Vector_Training_Time, n_chunks=50, random_seed=0):
+    """
+    Get 2x3 stain matrix. First row H and second row E.
+    :param Image: Input RGB image as a numpy array.
+    :param Stain_Vector_Lambda: Sparsity controlling parameter.
+    :param Stain_Vector_Training_Time: Not directly used due to scikit-learn's API, but you can adjust the 'n_iter' parameter of MiniBatchDictionaryLearning if needed.
+    :return: A 2x3 matrix of stain vectors for Hematoxylin and Eosin.
+    """
+    NUMBER_OF_STAINS = 2
+    # Assuming rgb2od and other necessary functions are defined elsewhere
+    OD = rgb2od(Image).reshape((INFERRED_DIMENSION, NUMBER_OF_COLORS))  # Reshape the optical density matrix
+    n_pixels = OD.shape[0]
+    chunk_size = (n_pixels + n_chunks - 1) // n_chunks
 
-#     OD = rgb2od(Image).reshape(INFERRED_DIMENSION, NUMBER_OF_COLORS)
+    # Initialize the MiniBatchDictionaryLearning model
+    model = MiniBatchDictionaryLearning(n_components=NUMBER_OF_STAINS,
+                                        alpha=Stain_Vector_Lambda,
+                                        max_iter=Stain_Vector_Training_Time,
+                                        fit_algorithm='lars',
+                                        positive_code=False,  # This option is not available, but post-processing could be applied
+                                        transform_algorithm='lasso_lars',
+                                        random_state=random_seed)
+    print('Starting to partial fit the model in chunks')
+    for i in tqdm(range(n_chunks)):
+        start = i * chunk_size
+        end = min((i + 1) * chunk_size, n_pixels)
+        if start >= end:
+            break  # No more data to process
+        OD_chunk = OD[start:end]
+        model.partial_fit(OD_chunk)
 
-#     print('Executing spams.lasso')
+    # # Fit the model and retrieve the components (stain vectors)
+    # model.fit(OD)
+    # StainVectors = model.components_
 
-#     DensityMapW           = spams.lasso(X       = OD.T,
-#                                         D       = StainMatrix.T,
-#                                         lambda1 = lamda,
-#                                         pos     = True)
+    # # Normalize each stain vector
+    # StainVectors = normalize_rows(StainVectors)
 
-#     return DensityMapW.toarray().T
+    # # Post-process StainVectors to ensure positivity if necessary
+    # # For example: StainVectors[StainVectors < 0] = 0
+
+    # # Assuming SortOutStainVectors function is defined elsewhere and performs necessary post-processing
+    # StainVectorOutput = SortOutStainVectors(StainVectors)
+
+    return StainVectorOutput
 
 
-def CalculateDensityMap(Image, StainMatrix, lamda):
+def CalculateDensityMap(Image, StainMatrix,lamda,n_chunks=50):
     """
     Get concentrations, a npix x 2 matrix
     :param I:
@@ -194,11 +185,49 @@ def CalculateDensityMap(Image, StainMatrix, lamda):
     """
 
     OD = rgb2od(Image).reshape(INFERRED_DIMENSION, NUMBER_OF_COLORS)
+    n_pixels = OD.shape[0]
+    chunk_size = (n_pixels + n_chunks - 1) // n_chunks
+    results = []
+    print("Executing spams.lasso in chunks")
+    for i in tqdm(range(n_chunks)):
+        start = i * chunk_size
+        end = min((i + 1) * chunk_size, n_pixels)
+        if start >= end:
+            break  # No more data to process
+        OD_chunk = OD[start:end]
+        DensityMapW = spams.lasso(X=OD_chunk.T, D=StainMatrix.T, lambda1=lamda, pos=True)
+        results.append(DensityMapW.toarray().T)
+    return np.vstack(results)
 
-    print('Executing Lasso')
+    # # Randomly sample pixels if too many
+    # if n_pixels > max_samples:
+    #     np.random.seed(random_seed)
+    #     idx = np.random.choice(n_pixels, max_samples, replace=False)
+    #     OD = OD[idx]
 
-    model = Lasso(alpha=lamda, positive=True)
-    model.fit(OD.T, StainMatrix.T)
-    DensityMapW = model.coef_.T
+    # print('Executing spams.lasso')
+    # DensityMapW           = spams.lasso(X       = OD.T,
+    #                                     D       = StainMatrix.T,
+    #                                     lambda1 = lamda,
+    #                                     pos     = True)
 
-    return DensityMapW
+    # return DensityMapW.toarray().T
+
+
+# def CalculateDensityMap(Image, StainMatrix, lamda):
+    # """
+    # Get concentrations, a npix x 2 matrix
+    # :param I:
+    # :param stain_matrix: a 2x3 six
+    # :return:
+    # """
+
+    # OD = rgb2od(Image).reshape(INFERRED_DIMENSION, NUMBER_OF_COLORS)
+
+    # print('Executing Lasso')
+
+    # model = Lasso(alpha=lamda, positive=True)
+    # model.fit(OD.T, StainMatrix.T)
+    # DensityMapW = model.coef_.T
+
+    # return DensityMapW
